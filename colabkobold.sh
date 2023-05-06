3674f0d352355c0749a1f55d8afb7096d41c6983
Science-student <122073403+Science-student@users.noreply.github.com>
2023-05-06 03:38:35 +0000

    Add files via upload

--- colabkobold.sh
+++ colabkobold.sh
@@ -113,16 +113,16 @@ if [ "$init" != "skip" ]; then
     cd /content
     if [ ! -z ${git+x} ]; then
         if [ "$git" == "Official" ]; then
-            git=https://github.com/koboldai/KoboldAI-Client
+            git=https://github.com/Science-student/KoboldAI-Client
         fi
         if [ "$git" == "United" ]; then
-            git=https://github.com/henk717/KoboldAI-Client
+            git=https://github.com/Science-student/KoboldAI-Client
         fi
         if [ "$git" == "united" ]; then
-            git=https://github.com/henk717/KoboldAI-Client
+            git=https://github.com/Science-student/KoboldAI-Client
         fi
     else
-        git=https://github.com/koboldai/KoboldAI-Client
+        git=https://github.com/Science-student/KoboldAI-Client
     fi
 
     mkdir /content/KoboldAI-Client
